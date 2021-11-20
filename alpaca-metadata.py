0409e630c0be1ada1c09524eb5fcb5198e7e7257
spenc <spencermayy@gmail.com>
2021-11-20 15:47:42 +0000

    Get shortable and fractionable assets from Alpaca

--- alpaca-metadata.py
+++ alpaca-metadata.py
@@ -1,11 +1,31 @@
-import alpaca_trade_api as tradeapi
-from config import alpaca_key, alpaca_secret
+# External Imports
+import alpaca_trade_api as alpaca
+from ratelimit import limits, sleep_and_retry
 
-api = tradeapi.REST(alpaca_key, alpaca_secret, api_version='v2')
+# Internal Imports
+from config import alpaca_key, alpaca_secret, alpaca_url
 
-# Get a list of all active assets.
-active_assets = api.list_assets(status='active')
+# Limit API Calls to 200 per minute
+calls = 200
+rate_limit = 60
 
-# Filter the assets down to just those on NASDAQ.
-nasdaq_assets = [a for a in active_assets if a.exchange == 'NASDAQ']
-print(nasdaq_assets)
+
+@sleep_and_retry
+@limits(calls=calls, period=rate_limit)
+def check_limit():
+    # Empty function to throttle calls to API
+    return None
+
+
+def get_alpaca_assets():
+    api = alpaca.REST(alpaca_key, alpaca_secret, alpaca_url)
+
+    # Get a list of all active assets.
+    active_assets = api.list_assets(status='active')
+
+    # Filter the assets down to just those on NASDAQ.
+    shortable_assets = [a for a in active_assets if a.shortable is True and a.fractionable is True]
+    print(shortable_assets)
+
+
+print(get_alpaca_assets())
