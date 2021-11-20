0b9c6220d786a3f9f9f99131898c236ec781530a
spenc <spencermayy@gmail.com>
2021-11-20 15:27:33 +0000

    Get active assets from alpaca

--- alpaca-metadata.py
+++ alpaca-metadata.py
@@ -1,9 +1,10 @@
 import alpaca_trade_api as tradeapi
+from config import alpaca_key, alpaca_secret
 
-api = tradeapi.REST()
+api = tradeapi.REST(alpaca_key, alpaca_secret, api_version='v2')
 
 # Get a list of all active assets.
-active_assets = api.list_assets(status='active', asset_class="us equity")
+active_assets = api.list_assets(status='active')
 
 # Filter the assets down to just those on NASDAQ.
 nasdaq_assets = [a for a in active_assets if a.exchange == 'NASDAQ']
