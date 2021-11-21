ffb606a14106a6c4f95b14c4046e19384ed0e5ca
Nathan May <nathanonex@gmail.com>
2021-11-21 23:23:41 +0000

    Remove secrets

--- app/config/config.py
+++ app/config/config.py
@@ -3,5 +3,5 @@ version = "v3"
 base_url = "https://financialmodelingprep.com/api/" + version + "/"
 
 alpaca_url = "https://api.alpaca.markets"
-alpaca_key = "AKR2ND7IJ7EMCZE0TRRP"
-alpaca_secret = "0AEtSWK2bdSFwuBzMK63TiLRQ6YQ1nXPr53EojIB"
+alpaca_key = "FOO"
+alpaca_secret = "BAR"
